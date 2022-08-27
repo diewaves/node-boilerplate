f36ba137d9f4eee5e2ccc35b095068c8046bd941
Diego <diedb234@gmail.com>
2022-08-27 16:00:42 +0200

    Fixed error meta data

--- app.js
+++ app.js
@@ -42,22 +42,37 @@ app.use((req, res, next) => {
 app.set('views', path.join(__dirname, 'views'))// Encuentra los archivos pug con las vistas
 app.set('view engine', 'pug')// Habilita el renderizado de pug
 
-app.get('/', async (req, res) => { // Renderizado normal
-  res.render('pages/home')
+// app.get('/', async (req, res) => { // Renderizado normal
+//   res.render('pages/home')
+// })
+
+app.get('/', async (req, res) => {
+  initApi(req).then(api => {
+    api.query(
+      Prismic.Predicates.any('document.type', ['meta', 'home'])).then(response => {
+      const { results } = response
+      const [meta, home] = results
+      // console.log(meta)
+      res.render('pages/home', {
+        meta,
+        home
+      })
+    })
+  })
 })
 
 app.get('/about', async (req, res) => { // Obtención de datos de prismic para cada vista
   initApi(req).then(api => {
     api.query(
       // Prismic.Predicates.at('document.type', 'about'), // Si quisieramos pasar un solo documento
-      Prismic.Predicates.any('document.type', ['about', 'meta'])).then(response => { // Este es el objeto de respuesta. Se renderiza la vista aquí
+      Prismic.Predicates.any('document.type', ['meta', 'about'])).then(response => { // Este es el objeto de respuesta. Se renderiza la vista aquí
       const { results } = response // Creamos un constructor. Desestructuración de javascript
-      const [about, meta] = results // Deconstruimos el array en un objeto
+      const [meta, about] = results // Deconstruimos el array en un objeto
       // console.log(results) // Esto devuelve un array con el objeto, sería lo mismo que response.results, si logeamos ahora about nos saca el objeto libre
       res.render('pages/about', {
         // document: response.results[0]
-        about,
-        meta // Al haber hecho la desestructuración, podemos colocar aqui el objeto directo
+        meta,
+        about // Al haber hecho la desestructuración, podemos colocar aqui el objeto directo
       })
     })
   })
